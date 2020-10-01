3293f101048d23a180a7f639e155086c49726e94
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-10-01 14:26:23 +0200

    Aggiunto file .sql per la creazioni delle tabelle, senza FK

---
+++ Documenti/creazioneDB.sql
@@ -0,0 +1,39 @@
+CREATE DATABASE gestione_apiario;
+USE gestione_apiario;
+CREATE TABLE utente(
+id INT NOT NULL PRIMARY KEY,
+nome_utente VARCHAR(40) NOT NULL,
+password_utente VARCHAR(40) NOT NULL,
+email_utente VARCHAR(40) NOT NULL
+);
+CREATE TABLE regina(
+id INT NOT NULL PRIMARY KEY,
+anno_nascita DATE NOT NULL,
+id_arnia INT NOT NULL
+);
+CREATE TABLE calendario(
+id INT NOT NULL PRIMARY KEY
+);
+CREATE TABLE annotazione(
+id INT NOT NULL PRIMARY KEY,
+data_inizo DATE NOT NULL,
+data_fine DATE NOT NULL,
+testo TEXT NOT NULL,
+id_arnia INT NOT NULL
+);
+CREATE TABLE meteo(
+id INT NOT NULL PRIMARY KEY,
+clima VARCHAR(50) NOT NULL,
+temperatura INT(2) NOT NULL,
+id_luogo INT NOT NULL
+);
+CREATE TABLE luogo(
+id INT NOT NULL PRIMARY KEY
+);
+CREATE TABLE arnia(
+id INT NOT NULL PRIMARY KEY,
+colore BOOLEAN NOT NULL,
+id_regina INT NOT NULL,
+id_utente INT NOT NULL,
+id_luogo INT NOT NULL
+);
