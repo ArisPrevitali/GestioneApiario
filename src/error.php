8c8751706a884399bae9ce4370fc5ef4494b8def
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-10-15 15:40:46 +0200

    Aggiunto file error.php per gestire errori

---
+++ src/error.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Gestione Apiario | Previtali Aris </title>
+    <link rel="icon" href="img/favicon.ico" type="image/gif">
+    <link rel="stylesheet" href="CSS/general.css">
+    <meta charset="utf-8">
+    <meta name="description" content="Pagina error gestione apiario">
+    <meta name="author" content="Aris Previtali">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <?php 
+        echo $_GET['pass'] . "<br>";
+        echo $_GET['name'] . "<br>";
+        echo $_GET['email'] . "<br>";
+    ?>
+</body>
+</html>
