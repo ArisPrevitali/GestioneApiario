d29cd43168bc4bf882aac72781e83972d9b26829
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-10-15 14:25:20 +0200

    Update pagine di login

---
+++ src/login.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Gestione Apiario | Previtali Aris </title>
+    <link rel="icon" href="img/favicon.ico" type="image/gif">
+    <link rel="stylesheet" href="CSS/general.css">
+    <meta charset="utf-8">
+    <meta name="description" content="Pagina login gestione apiario">
+    <meta name="author" content="Aris Previtali">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+    </header>
+    <div class="body center">
+        <h1 class="title">Gestione Apiario - Registrazione</h1>
+        <h3 class="text">Bentornato!</h3>
+        <form action="#" method="post" class="center">
+            <p>Nome: </p>
+            <input type="text" class="textbox" name="name">
+            <p class="block">Password: </p>
+            <input type="text" class="textbox" name="password">
+            <input type="submit" class="textbox button" name="submit" value="INVIO">
+        </form>
+    </div>
+    <?php include "footer.html"?>
+</body>
+</html>
