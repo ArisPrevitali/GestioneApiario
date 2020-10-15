6535e0ee0c025b3462ce31fc09a8aa29ddbcc9a3
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-10-15 14:24:50 +0200

    Aggiunta pagina home (non finita)

---
+++ src/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Gestione Apiario | Previtali Aris </title>
+    <link rel="icon" href="img/favicon.ico" type="image/gif">
+    <link rel="stylesheet" href="CSS/general.css">
+    <link rel="stylesheet" href="CSS/home.css">
+    <meta charset="utf-8">
+    <meta name="description" content="Pagina login gestione apiario">
+    <meta name="author" content="Aris Previtali">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+        <p class="rightTop">nome utente + numero apiario</p>
+    </header>
+    <div class="body">
+        <h1 class="title center">Gestione Apiario - Home</h1>
+        <!--Aggiungi arnia.file-->
+        <input type="button" class="button" name="addArnie" value="Aggiungi un'arnia" onclick="">
+    </div>
+    <?php include "footer.html"?>
+</body>
+</html>
