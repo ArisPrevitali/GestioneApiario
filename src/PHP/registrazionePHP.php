74520f8f1591d4bf1a95ee0574733f7fe05fcb6f
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-10-22 15:42:49 +0200

    Aggiornato file php per la registrazione, aggiunte query sql

--- src/PHP/registrazionePHP.php
+++ src/PHP/registrazionePHP.php
@@ -15,6 +15,7 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $passVal = false;
     $nameVal = false;
     $emailVal = false;
+    $passSame = false;
     $name = strval($_POST['nome']);
     $name = test_input($name);
     if(!preg_match('/^[a-zA-Z0-9]+$/i', $name)){
@@ -31,40 +32,61 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     }else{
         $emailVal = true;
     }
-
+    $pattern = "/\W|_/";
     $pass = test_input($_POST["password"]);
     $confPass = test_input($_POST["confPass"]);
     if(!empty($pass)){
-        if(strlen($pass) <= 6){
-            $passErr .= "Deve avere più di 6 caratteri"; 
-        }else if(preg_match('[@_!#$%^&*()?/|}{~:]', $pass)){
-            $passErr .= ", deve contenere almeno un carattere speciale";
-        }else if(preg_match('[a-z]', $pass)){
-            $passErr .= ", deve contenere almeno una lettera minuscolo";
-        }else if(preg_match('[A-Z]', $pass)){
-            $passErr .= ", deve contenere almeno una lettera maiuscola";
-        }else{
-            if($pass === $confPass){
-                $passVal = true;
+        if($pass === $confPass){
+            $passSame = true;
+            if(strlen($pass) >= 6){
+                if(preg_match($pattern, $pass)){
+                    if(preg_match('/[a-z]+/', $pass)){
+                        if(preg_match('/[A-Z]+/', $pass)){
+                            $passVal = true;
+                        }else{
+                            $passErr .= ", deve contenere almeno una lettera maiuscola.";
+                        }
+                    }else{
+                        $passErr .= ", deve contenere almeno una lettera minuscolo";
+                    }
+                }else{
+                    $passErr .= ", deve contenere almeno un carattere speciale";
+                }
             }else{
-                $passErr .= ", le due password non corrispondono";
+                $passErr .= "Deve avere più di 6 caratteri";
             }
+        }else{
+            $passErr = "Le due password non corrispondono";
         }
     }else{
-        $passErr = "Scrivere la password.";
+        $passErr = "Scrivere la pass";
     }
+    echo $passErr;
     if($passVal && $nameVal && $emailVal){
-        echo "Valori validi";
+        //SQL
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "gestione_apiario";
+    
+        $conn = new mysqli($servername, $username, $password, $dbname);
+    
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+    
+        $sql = "INSERT INTO utente (nome_utente, password_utente, email_utente)
+            VALUES ('$name', '$pass', '$email')";
+    
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        $conn->close();
+        //header("location: /login.php");
     }else{
-        if(!empty($passErr)){
-            echo alert($passErr);
-        }
-        if(!empty($nameErr)){
-            echo alert($nameErr);
-        }
-        if(!empty($emailErr)){
-            echo alert($emailErr);
-        }
+        //header('location: /error.php?pass='.$passVal.'&name='.$nameVal.'&email='.$emailVal.'&passSame='.$passSame);
     }
 }
 ?>
