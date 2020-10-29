bdf772f31fd8bfcd94eda808daecbadc5ff394d1
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-10-29 13:17:17 +0100

    Aggiunto pagina per poter aggiungere un'arnia

---
+++ src/aggiungiArnia.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title> Gestione Apiario | Previtali Aris </title>
+    <link rel="icon" href="img/favicon.ico" type="image/gif">
+    <link rel="stylesheet" href="CSS/general.css">
+    <link rel="stylesheet" href="CSS/home.css">
+    <meta charset="utf-8">
+    <meta name="description" content="Pagina aggiungi arnia gestione apiario">
+    <meta name="author" content="Aris Previtali">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+    </header>
+    <div class="body">
+        <h1 class="title center">Gestione Apiario - Aggiungi Arnia</h1>
+        <input type="button" class="buttonCenter" name="addArnie" value="Aggiungi un'arnia" onclick="">
+        <!--Aggiungi arnia.file-->
+        <table>
+            <tr>
+                <th>Luogo</th>
+                <th>Colore</th>
+                <th>Note</th>
+            </tr>
+            <tr>
+                <!--Inserire dati dal DB max 5-->
+            </tr>
+        </table>
+        
+    </div>
+    <?php include "footer.html"?>
+</body>
+</html>
