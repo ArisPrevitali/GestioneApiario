8c2f3e8c8f9fdce0424bff9d9c2ae6e540386506
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-11-12 14:47:20 +0100

    Modificato file per l'aggiunta arnia, upgrade funzioni

--- src/PHP/arniaPHP.php
+++ src/PHP/arniaPHP.php
@@ -1,4 +1,5 @@
 <?php 
+session_start();
 function test_input($data) {
     $data = trim($data);
     $data = stripslashes($data);
@@ -7,24 +8,48 @@ function test_input($data) {
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $luogo = isset($_POST['luogo']);
-    $color = isset($_POST['color']);
-    $annoRegina = isset($_POST['annoRegina']);
-    $note = isset($_POST['note']);
-    $abitata = isset($_POST['abitata']);
-    $luogo = test_input($luogo);
-    $color = test_input($color);
-    $annoRegina = test_input($annoRegina);
-    $note = test_input($note);
-    $abitata = test_input($abitata);
+    if(isset($_POST['luogo'])){
+        $luogo = test_input($_POST['luogo']);
+    } 
+    if(isset($_POST['color'])){
+        $color = test_input($_POST['color']);
+    }
+    if(isset($_POST['annoRegina'])){
+        $annoRegina = test_input($_POST['annoRegina']);
+        echo $annoRegina . "SUS" . "<br>";
+    }
+    if(isset($_POST['note'])){
+        $note = test_input($_POST['note']);
+    }
+    if(isset($_POST['abitata'])){
+        $abitata = test_input($_POST['abitata']);
+    }
     include "connectionMYSQL.php";
+    
+    //ricavo l'id
+    $sqlGetIdUser = "SELECT id FROM utente
+    WHERE nome_utente = ?";
+    $stmt = $conn->prepare($sqlGetIdUser);
+    $stmt->bind_param("s", $_SESSION["nameUser"]);
+    $stmt->execute();
+    $stmt->bind_result($id);
+    $stmt->fetch();
+    $stmt->close();
+
+    //aggiungere regina per l'utente
+    if($annoRegina != "" && isset($_SESSION['nameUser'])){
+        include "reginaPHP.php";
+    }
+
+    //aggiunta luogo
+    if($luogo != "" && isset($_SESSION['nameUser'])){
+        include "luogoPHP.php";
+    }
     if($abitata != ""){
         if($luogo != "" && $color != "" && $annoRegina != "" && $note != ""){
             $sql = "INSERT INTO arnia(colore, testo, abitata, id_regina, id_utente, id_luogo)
-                    VALUES ()";
+                    VALUES (?, ?, ?, ?,)";
         }
-    }else if($luogo != "" && $color != "" && $note != ""){
-
     }
 }
 ?>
