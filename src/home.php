9974b1a2840bed9a76a6daff4ab4640a17dcfd10
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-11-12 14:35:19 +0100

    Modificato tipo di collegamento e aggiunta sessione

--- src/home.php
+++ src/home.php
@@ -6,23 +6,23 @@
     <link rel="stylesheet" href="CSS/general.css">
     <link rel="stylesheet" href="CSS/home.css">
     <meta charset="utf-8">
-    <meta name="description" content="Pagina login gestione apiario">
+    <meta name="description" content="Pagina home gestione apiario">
     <meta name="author" content="Aris Previtali">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
     <header>
-        <p class="rightTop">nome utente + numero apiario</p>
+        <p class="rightTop"><?php echo $_GET['name']; ?></p>
     </header>
     <div class="body">
         <h1 class="title center">Gestione Apiario - Home</h1>
-        <input type="button" class="button" name="addArnie" value="Aggiungi un'arnia" onclick="">
+        <input type="button" class="buttonCenter" name="addArnie" value="Aggiungi un'arnia" onclick="location.href = 'aggiungiArnia.php?name=<?php echo $_GET['name'] ?>'">
         <!--Aggiungi arnia.file-->
         <table>
             <tr>
-                <th>nomecolonna (luogo)</th>
-                <th>nomecolonna (colore)</th>
-                <th>nomecolonna (note)</th>
+                <th>Luogo</th>
+                <th>Colore</th>
+                <th>Note</th>
             </tr>
             <tr>
                 <!--Inserire dati dal DB max 5-->
