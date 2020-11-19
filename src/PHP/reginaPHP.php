d5ad71fb4d434ce0268fdf8cce28eccf1663e637
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-11-19 13:37:09 +0100

    Pagina aggiunta regina, inutilizzata

--- src/PHP/reginaPHP.php
+++ src/PHP/reginaPHP.php
@@ -1,12 +1,26 @@
+//inutilizzato
+
 <?php 
+include "connectionMYSQL.php";
 //aggiungo la regina tramite l'id dell'utente
 if(isset($id)){
+    $annoRegina = date_create()->format('Y-m-d');
     $sql = "INSERT INTO regina(anno_nascita, id_utente)
-            VALUES ($annoRegina, $id)";
-    if ($conn->query($sql) === TRUE) {
+            VALUES (?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("si", $annoRegina, $id);
+    $stmt->execute();
+    $stmt->close();
+    $_SESSION['idRegina'] = $conn->insert_id;
+    if($lastIdRegina===false){
+        header("location: /errorLogin.php");
+    }else{
+        header("location: /PHP/arniaPHP.php");
+    }
+    /**if ($conn->query($sql) === TRUE) {
         //echo "New record created successfully" . "<br>";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+        echo $conn->error;
+    }*/
 }
 ?>
