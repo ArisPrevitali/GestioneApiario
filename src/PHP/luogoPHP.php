298f5bef78ab1435166eac490c5560deb705a75d
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-11-19 13:36:57 +0100

    Pagina aggiunta luogo, inutilizzata

--- src/PHP/luogoPHP.php
+++ src/PHP/luogoPHP.php
@@ -1,12 +1,26 @@
+//inutilizzato
+
 <?php 
+include "connectionMYSQL.php";
 //aggiungo il luogo tramite id dell'utente
 if(isset($id)){
     $sql = "INSERT INTO luogo(luogo)
-            VALUES ($luogo";
-    if ($conn->query($sql) === TRUE) {
+            VALUES (?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $luogo);
+    $stmt->execute();
+    $stmt->close();
+    $_SESSION['idLuogo'] = $conn->insert_id;
+    echo $lastIdLuogo;
+    if($lastIdLuogo===false){
+        header("location: /errorLogin.php");
+    }else{
+        header("location: /PHP/arniaPHP.php");
+    }
+    /**if ($conn->query($sql) === TRUE) {
         //echo "New record created successfully" . "<br>";
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
-    }
+    }*/
 }
 ?>
