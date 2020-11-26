522778b5ec429942cacc6940830bf0588e7b99e4
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-11-26 15:35:33 +0100

    Add SQL query

--- src/PHP/modificaArniaPHP.php
+++ src/PHP/modificaArniaPHP.php
@@ -1,5 +1,8 @@
 <?php 
 session_start();
+$arrayTable = array();
+$datiArnia = array();
+$_SESSION['datiArnia'] = $datiArnia;
 include "connectionMYSQL.php";
 function test_input($data) {
     $data = trim($data);
@@ -17,13 +20,37 @@ $stmt->bind_result($id);
 $stmt->fetch();
 $stmt->close();
 
+
+if(!isset($_SESSION['tableArnia'])){
+    $sqlGetTable = "SELECT COLUMN_NAME
+        FROM INFORMATION_SCHEMA.COLUMNS
+        WHERE TABLE_SCHEMA='gestione_apiario' 
+        AND TABLE_NAME='arnia'";
+    $result = $conn->query($sqlGetTable);
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $arrayTable[] = $row['COLUMN_NAME'];
+        }
+        $_SESSION['tableArnia'] = $arrayTable;
+        header('location: ./../modificaArnia.php');
+    }
+}
+$arrayresult = array();
 $sql = "SELECT * FROM arnia
         WHERE id_utente = ?";
 $stmt = $conn->prepare($sql);
 $stmt->bind_param("i", $id);
 $stmt->execute();
-$stmt->bind_result($id, $colore, $testo, $abitata, $id_regina, $id_utente, $id_luogo);
-$stmt->fetch();
+$result = $stmt->get_result();
+while($row = $result->fetch_assoc()){
+    //print_r($row);
+    $arrayresult[] = $row;    
+}
+$_SESSION['datiArnia'] = $arrayresult;
 $stmt->close();
-echo $id . "-" . $id_luogo . "-" . $colore . "-" . $testo . "<br>";
+
+if(isset($_GET['id'])){
+    $id = $_GET['id'];
+    
+}
 ?>
