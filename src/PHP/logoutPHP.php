7d2052b99171cf756b8a3ff2b5bcf15bdd90b340
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-11-26 15:35:12 +0100

    Update session_destroy();

--- src/PHP/logoutPHP.php
+++ src/PHP/logoutPHP.php
@@ -1,8 +1,9 @@
 <?php  
 session_start();
 if(isset($_POST["logout"])){
-    unset($_SESSION["nameUser"]);
-    unset($_SESSION["loggedUser"]);
+    //unset($_SESSION["nameUser"]);
+    //unset($_SESSION["loggedUser"]);
+    session_destroy();
     header("location: /home.php");
 }
 ?>
