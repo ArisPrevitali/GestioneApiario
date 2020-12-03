13a48fd7a0e29c4e5e58fa831e4a0458c2cba5e9
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-12-03 15:39:23 +0100

    Update header

--- src/modificaArnia.php
+++ src/modificaArnia.php
@@ -16,11 +16,13 @@ session_start();
 </head>
 <?php if(isset($_SESSION["loggedUser"])): ?>
 <body>
-    <header>
-        <p class="rightTop">Nome: <?php if(isset($_SESSION["nameUser"])): echo $_SESSION["nameUser"]; endif ?></p>
-        <form action="<?php echo 'PHP/logoutPHP.php' ?>" method="POST" class="">
-            <input type="submit" name="logout" value="LOGOUT" class="button rightTop">
-        </form>
+<header class="head">
+        <div class=" inline-right">
+            <form action="<?php echo 'PHP/logoutPHP.php' ?>" method="POST">
+                <label for="logout-btn">Nome: <?php if(isset($_SESSION["nameUser"])): echo $_SESSION["nameUser"]; endif ?></label>
+                <input class=" logout-btn" type="submit" name="logout" value="LOGOUT" id="logout-btn">
+            </form>
+        </div>
     </header>
     <div class="body">
         <h1 class="title center">Gestione Apiario - Modifica Arnia</h1>
@@ -37,40 +39,87 @@ session_start();
                             echo "</th>";
                         }
                     }
-                    echo "<th>";
-                    echo "Modifica";
-                    echo "</th>";
-                    echo "<th>";
-                    echo "Elimina";
-                    echo "</th>";
-                    echo "</tr>";
-                    foreach($_SESSION['datiArnia'] as $value){
-                        echo "<tr>";
-                        $count = 0;
-                        foreach($value as $assoc => $val){
-                            if(stripos($assoc, "id_") === false){
-                                if($count == 0){
-                                    $id = $val;
-                                    $count++;
-                                }
-                                echo "<td>";
-                                echo $val;
-                                echo "</td>";
-                            }
-                        }
-                        echo "<td>";
-                        echo "<a href='./PHP/modificaArniaPHP.php?id=" . "$id'>Modifica</a>";
-                        echo "</td>";
+                    if(!isset($_GET['modifica'])){
+                        echo "<th>";
+                        echo "Modifica";
+                        echo "</th>";
+                        echo "<th>";
+                        echo "Elimina";
+                        echo "</th>";
                         echo "</tr>";
+                        foreach($_SESSION['datiArnia'] as $value){
+                            echo "<tr>";
+                            $count = 0;
+                            foreach($value as $assoc => $val){
+                                if(stripos($assoc, "id_") === false){
+                                    if($count == 0){
+                                        $id = $val;
+                                        $count++;
+                                    }
+                                    echo "<td>";
+                                    echo $val;
+                                    echo "</td>";
+                                }
+                            }
+                            echo "<td>";
+                            echo "<a href='./PHP/modificaArniaPHP.php?id=" . "$id'>Modifica</a>";
+                            echo "</td>";
+                            echo "<td>";
+                            echo "<a href='./PHP/eliminaArniaPHP.php?id=" . "$id'>X</a>";
+                            echo "</td>";
+                            echo "</tr>";
+                        } 
+                    }else{
+                        foreach($_SESSION['datiModifica'] as $value){
+                            echo "<tr>";
+                            $count = 0;
+                            foreach($value as $assoc => $val){
+                                if(stripos($assoc, "id_") === false){
+                                    if($count == 0){
+                                        $id = $val;
+                                        $count++;
+                                    }
+                                    echo "<td>";
+                                    echo $val;
+                                    echo "</td>";
+                                }
+                            }
+                            echo "</tr>";
+                        }
+                        ?>
+                        <form action="<?php if($_SESSION['loggedUser']): echo 'PHP/updateArnia.php?$id="."$id'; endif ?>" method="POST" class="float-left center">
+                            <p>Luogo (opzionale):</p>
+                            <input type="text" name="luogo">
+                            <p>Colore (opzionale):</p>
+                            <select name="color">
+                                <option>Bianco</option>
+                                <option>Grigio</option>
+                                <option>Rosso</option>
+                                <option>Verde</option>
+                                <option>Giallo</option>
+                                <option>Azzurro</option>
+                                <option>Blu</option>
+                                <option>Rosa</option>
+                                <option>Viola</option>
+                                <option>Marrone</option>
+                                <option>Arancione</option>
+                                <option selected="selected">Non colorata</option>
+                            </select>
+                            <p>Note (opzionale):</p>
+                            <input type="text" name="note">
+                            <p>Abitata? (opzionale):</p>
+                            <input type="checkbox" name="abitata">
+                            <p>Anno regina (opzionale):</p>
+                            <input type="date" name="annoRegina">
+                            <br>
+                            <input type="submit" class="buttonCenter" name="submit" value="MODIFICA">
+                        </form>
+                    <?php
                     }
                 }
                 ?>
             </table>
         </div>
-        
-        <form> 
-            
-        </form>
     </div>
     <?php include "footer.html"?>
 </body>
