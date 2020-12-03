1be37590a7a1c01be1773fd6c773e7320fbbd198
ArisPrevitali <aris.previtali@samtrevano.ch>
2020-12-03 15:41:16 +0100

    Add ricavo i dati dell'arnia di un'utente

---
+++ src/PHP/datiPHP.php
@@ -0,0 +1,19 @@
+<?php
+include "connectionMYSQL.php";
+$arrayDatiLog = array();
+//Cerco l'utente loggato
+include "getUtenteLog";
+
+//Dati delle sue arnie
+$sql = "SELECT nome_luogo, colore, testo FROM arnia
+        WHERE id_utente = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $id_utente);
+$stmt->execute();
+$result = $stmt->get_result();
+while($row = $result->fetch_assoc()){
+    $arrayDatiLog[] = $row;
+}
+$_SESSION['datiLogHome'] = $arrayDatiLog;
+$stmt->close();
+?>
