ea08f64177a84e18503ad44ead8800c588c76123
Aris Previtali <aris.previtali@samtrevano.ch>
2020-12-17 15:17:12 +0100

    Add tabella annotazione, Update tabelle

--- Documenti/creazioneDB.sql
+++ Documenti/creazioneDB.sql
@@ -1,52 +1,57 @@
+DROP DATABASE IF EXISTS gestione_apiario;
 CREATE DATABASE gestione_apiario;
 USE gestione_apiario;
 CREATE TABLE utente(
-id INT NOT NULL PRIMARY KEY,
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 nome_utente VARCHAR(40) NOT NULL,
 password_utente VARCHAR(40) NOT NULL,
 email_utente VARCHAR(40) NOT NULL
 );
 CREATE TABLE regina(
-id INT NOT NULL PRIMARY KEY,
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 anno_nascita DATE NOT NULL,
-id_arnia INT NOT NULL
+id_utente INT NOT NULL
 );
 CREATE TABLE calendario(
-id INT NOT NULL PRIMARY KEY
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY
 );
-CREATE TABLE annotazione(
-id INT NOT NULL PRIMARY KEY,
-data_inizo DATE NOT NULL,
+CREATE TABLE trattamento(
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+data_inizio DATE NOT NULL,
 data_fine DATE NOT NULL,
-testo TEXT NOT NULL,
+testo TEXT,
 id_arnia INT NOT NULL
 );
 CREATE TABLE meteo(
-id INT NOT NULL PRIMARY KEY,
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
 clima VARCHAR(50) NOT NULL,
 temperatura INT(2) NOT NULL,
-id_luogo INT NOT NULL
+nome_luogo varchar(40) NOT NULL
 );
 CREATE TABLE luogo(
-id INT NOT NULL PRIMARY KEY
+/**id INT AUTO_INCREMENT NOT NULL,*/
+luogo VARCHAR(40) NOT NULL,
+PRIMARY KEY(luogo)
 );
 CREATE TABLE arnia(
-id INT NOT NULL PRIMARY KEY,
-colore BOOLEAN NOT NULL,
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+colore ENUM('Bianco', 'Grigio', 'Rosso', 'Giallo', 'Verde', 'Azzurro', 'Blu', 'Rosa', 'Viola', 'Marrone', 'Arancione'),
+testo TEXT,
+abitata BOOLEAN NOT NULL,
+nome_luogo varchar(40) NOT NULL,
 id_regina INT NOT NULL,
-id_utente INT NOT NULL,
-id_luogo INT NOT NULL
+id_utente INT NOT NULL
 );
-
-ALTER TABLE regina
-ADD FOREIGN KEY (id_arnia) REFERENCES arnia(id);
-ALTER TABLE annotazione
-ADD FOREIGN KEY (id_arnia) REFERENCES arnia(id);
-ALTER TABLE meteo
-ADD FOREIGN KEY (id_luogo) REFERENCES luogo(id);
-ALTER TABLE arnia
-ADD FOREIGN KEY (id_regina) REFERENCES regina(id);
-ALTER TABLE arnia
-ADD FOREIGN KEY (id_utente) REFERENCES utente(id);
-ALTER TABLE arnia
-ADD FOREIGN KEY (id_luogo) REFERENCES luogo(id);
+CREATE TABLE annotazione(
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+data_ann DATE NOT NULL,
+testo TEXT,
+id_arnia INT NOT NULL
+);
+ALTER TABLE regina ADD FOREIGN KEY (id_utente) REFERENCES utente(id);
+ALTER TABLE trattamento ADD FOREIGN KEY (id_arnia) REFERENCES arnia(id);
+ALTER TABLE meteo ADD FOREIGN KEY (nome_luogo) REFERENCES luogo(luogo);
+ALTER TABLE arnia ADD FOREIGN KEY (id_regina) REFERENCES regina(id);
+ALTER TABLE arnia ADD FOREIGN KEY (id_utente) REFERENCES utente(id);
+ALTER TABLE arnia ADD FOREIGN KEY (nome_luogo) REFERENCES luogo(luogo);
+ALTER TABLE annotazione ADD FOREIGN KEY (id_arnia) REFERENCES arnia(id);
